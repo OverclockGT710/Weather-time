96555e7317ca6979b58f45002438b08e3449739d
overclockgt710 <alex.cusack2004@gmail.com>
2020-10-09 15:30:22 +1100

    more data

--- Index.py
+++ Index.py
@@ -5,7 +5,11 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from src.navbar import navbar
+from src.Dataservice import get_data
 
+df = get_data()
+
+print(df.head())
 
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 
