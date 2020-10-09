265670229d6469016f7f0259a96447714af33a15
overclockgt710 <alex.cusack2004@gmail.com>
2020-10-09 15:12:59 +1100

    Added Nav bar componet

--- Index.py
+++ Index.py
@@ -4,14 +4,13 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-
+from src.navbar import navbar
 
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-   
+   html.Div(navbar)
+   ,
 ])
 
 if __name__ == "__main__":
---
+++ src/navbar.py
@@ -0,0 +1,10 @@
+import dash_bootstrap_components as dbc 
+
+
+navbar = dbc.NavbarSimple(
+    
+    brand="Weather Data",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
